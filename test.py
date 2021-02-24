c82e914d1caa2a30bca68e06616c2b8fd8735553
Zehra Ballı <balli.zehra@gmail.com>
2021-02-24 21:57:57 +0300

    library

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-test2
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.datasets import make_classification
